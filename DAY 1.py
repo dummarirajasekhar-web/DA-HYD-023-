88824f4d8f8789a6df4dfa638bea95fa94cc5a27
dummarirajasekhar-web <dummarirajasekhar@gmail.com>
2026-07-28 13:32:02 +0530

    Tokens

---
+++ DAY 1.py
@@ -0,0 +1,30 @@
+#print('Hello codegnan')
+# is used as sigle line comment
+'''
+tokens -- snallest units in program. there are 5 tokens,
+keywords , identifiers, literals , operators , punctuators
+'''
+#keywords-- these meant to be reserved words.
+name = 'raj'
+print(name)
+email_id = "dummarirajasekhar@gmail.com"
+print(email_id)
+R = "hr"
+print(R)
+#literals -- these are constants (value(s))-->nnumber.character
+
+#operators --> operators perform specific operation --> 7 types 
+length = 4
+breadth = 3
+#area = length * breadth
+print(area)
+
+#singleassignment variable
+name="raj"
+print(name)
+
+#multiassognment of variables
+name, location, age = "raj","hyd",21
+print(name)
+print(location)
+print (age)
