548cc2cb59da1ae6fcc3c4d40b1bc514d87ad082
dummarirajasekhar-web <dummarirajasekhar@gmail.com>
2026-08-06 13:02:45 +0530

    loops practice

---
+++ TASK.py
@@ -0,0 +1,67 @@
+'''
+total = 0
+price = list(map(int,input("enter the price: ").split(',')))
+for i in price :
+   total = total + i   
+print("total amount is : ",total)
+'''
+# FINDING UPPER,LOWER ,DIGITS AND SP_CHAR IN A PASSWORD
+
+"""password = input("enter the password : ")
+upper_case=0
+lower_case= 0
+special_char=0
+digit=0
+
+for i in password:
+    if "A" <=i<="Z":
+        upper_case +=1
+    elif "a" <=i <= "z":
+        lower_case +=1
+    elif "1"<= i <='9':
+        digit +=1
+    else :
+        special_char+=1
+print("upper :", upper_case)
+print("lower :", lower_case)
+print("digit :", digit)
+print("special: ", special_char)    
+"""
+#printing or spliting of mail
+
+"""
+username = input("enter the username: ").split()
+for i in username:
+    print(i.split('@')[1])
+    
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
